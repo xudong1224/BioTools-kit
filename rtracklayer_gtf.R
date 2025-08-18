2b5cd6c7b94453a550db955fa16e06ac1da50eff
xudong1224 <920546413@qq.com>
2025-08-18 16:30:03 +0800

    Add export gtf code

--- rtracklayer_gtf.R
+++ rtracklayer_gtf.R
@@ -3,52 +3,49 @@
 library(rtracklayer)
 library(dplyr)
 
-setwd("C:/Users/DELL/Desktop/gtf")
+setwd("C:/Users/DELL/Desktop/")
 
-gtf <- rtracklayer::import('gencode.v46.annotation.gtf')
-gtf<-as.data.frame(gtf)
+gtf <- rtracklayer::import('Rattus_norvegicus.Rnor_6.0.104.chr.gtf')
+
+col_target <- grep("gene.*type", names(test), value = TRUE) #字符呈现
+
+protein_coding.gtf <- gtf |> 
+  as.data.frame() |> 
+  filter(.data[[col_target]] == "protein_coding")
+
+protein_coding.gtf <- makeGRangesFromDataFrame(
+  protein_coding.gtf,
+  keep.extra.columns = TRUE,
+  ignore.strand = FALSE
+)
+
+export(protein_coding.gtf, con = "Rattus_norvegicus_protein_coding.gtf", format = "GTF")
 
 #获取编码蛋白
-protein_coding.gtf <- gtf %>%
-  dplyr::select(c("gene_id", "gene_name", "gene_type")) %>%
-  arrange(gene_id) %>%   
-  distinct(gene_name, .keep_all = T) %>%
-  filter(gene_type == "protein_coding")
 
+#先提取蛋白编码基因，再去重复
+
+protein_coding.gtf <- gtf |> 
+  filter(type == "gene",gene_type == "protein_coding") |> 
+  distinct(gene_name,.keep_all = T) |> 
+  dplyr::select(c("gene_id","gene_name","gene_type"))
+  
 #同理可以获取lnRNA、miRNA
-lncRNA.gtf<-gtf %>% 
-  dplyr::select(c("gene_id","gene_name","gene_type")) %>%   
-  arrange(gene_id) %>%   
-  distinct(gene_name,.keep_all = T) %>%   
-  filter(gene_type == "lncRNA")
 
-miRNA.gtf<-gtf %>%   
-  dplyr::select(c("gene_id","gene_name","gene_type")) %>%   
-  arrange(gene_id) %>%   
-  distinct(gene_name,.keep_all = T) %>%
-  filter(gene_type == "miRNA")
+lncRNA.gtf <- gtf |> 
+  filter(type == "gene",gene_type == "lncRNA") |>
+  distinct(gene_name,.keep_all = T) |> 
+  dplyr::select(c("gene_id","gene_name","gene_type"))
+
+miRNA.gtf <- gtf |> 
+  filter(type == "gene",gene_type == "miRNA") |>
+  distinct(gene_name,.keep_all = T) |> 
+  dplyr::select(c("gene_id","gene_name","gene_type"))
 
 #获取基因symtol和对应的基因类型
-gene_symtol.gtf <- gtf %>%
-  dplyr::select(c("gene_name", "gene_type")) %>%
-  arrange(gene_name) %>%   
-  distinct(gene_name, .keep_all = T)
+# 同一个 gene_name 对应多个 gene_type 时，不能只对 gene_name 去重，否则会丢失部分类型信息
 
-
-
-
-#对基因表达谱进行注释
-library(readr)
-
-gene_matrix_fpkm <- read_delim("gene_count_matrix.fpkm.xls",delim = "\t",
-                                     escape_double = FALSE,trim_ws = TRUE)
-
-colnames(gene_matrix_fpkm)[1] <- "gene_name"
-
-fpkm <- gene_matrix_fpkm |> 
-  left_join(gene_symtol.gtf,by = "gene_name") |> 
-  dplyr::select(gene_name,gene_type,everything())
-
-write.table(fpkm,"gene_matrix.fpkm.xls",row.names = F,sep = "\t")
-
-
+gene_symtol.gtf <- gtf |> 
+  dplyr::select(c("gene_name", "gene_type")) |> 
+  arrange(gene_name,gene_type) |> 
+  distinct(gene_name,gene_type, .keep_all = T)
